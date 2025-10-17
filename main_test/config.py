a2de49ac20d564732ba2b3150c99e501261c1c44
Артём Голубцов <gaa.771@gmail.com>
2025-10-17 23:38:50 +0300

    Add files via upload

---
+++ main_test/config.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from settings import BASE_URL_1, BASE_URL_2, BASE_URL_3, API_KEY
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()))
+    yield driver
+    driver.quit()
+
+
+# Авторизация
+@pytest.fixture
+def headers():
+    return {
+        "X-API-KEY": API_KEY,
+        "accept": 'application/json'
+    }
--- main_test/test/test_api.py
+++ main_test/test/test_api.py
@@ -1,19 +1,11 @@
 import requests
 import pytest
 from settings import BASE_URL_1, BASE_URL_2, BASE_URL_3, API_KEY
-
-
-# Авторизация
-@pytest.fixture
-def headers():
-    return {
-        "X-API-KEY": API_KEY,
-        "accept": 'application/json'
-    }
+from config import headers
 
 
 # Позитивный тест - поиск на кириллице
-def test_find_cyrillic_positive(headers):
+def test_find_cyrillic_positive_api(headers):
     base_url_loc = BASE_URL_1 + "Аватар"
     response = requests.get(base_url_loc, headers=headers)
 
@@ -21,7 +13,7 @@ def test_find_cyrillic_positive(headers):
 
 
 # Позитивный тест - поиск на латинице
-def test_find_latin_positive(headers):
+def test_find_latin_positive_api(headers):
     base_url_loc = BASE_URL_1 + "Warcraft"
     response = requests.get(base_url_loc, headers=headers)
 
@@ -29,7 +21,7 @@ def test_find_latin_positive(headers):
 
 
 # Позитивный тест - поиск по названию с цифрами
-def test_find_figure_positive(headers):
+def test_find_figure_positive_api(headers):
     base_url_loc = BASE_URL_1 + "Форсаж 4"
     response = requests.get(base_url_loc, headers=headers)
 
@@ -37,7 +29,7 @@ def test_find_figure_positive(headers):
 
 
 # Негативный тест - поиск фильма с пустым названием
-def test_find_empty_negative(headers):
+def test_find_empty_negative_api(headers):
     base_url_loc = BASE_URL_2 + "name=null"
     response = requests.get(base_url_loc, headers=headers)
 
@@ -45,7 +37,7 @@ def test_find_empty_negative(headers):
 
 
 # Негативный тест - поиск фильма со спецсимволами в названии
-def test_find_special_character_negative(headers):
+def test_find_special_character_negative_api(headers):
     base_url_loc = BASE_URL_2 + "name=#"
     response = requests.get(base_url_loc, headers=headers)
 
@@ -53,7 +45,7 @@ def test_find_special_character_negative(headers):
 
 
 # Негативный тест - поиск фильма с датой выпуска из будущего
-def test_find_date_future_negative(headers):
+def test_find_date_future_negative_api(headers):
     base_url_loc = BASE_URL_3 + "year=2100"
     response = requests.get(base_url_loc, headers=headers)
 
@@ -62,7 +54,7 @@ def test_find_date_future_negative(headers):
 
 # Негативный тест - поиск фильма с использованием максимально
 # возможного интервала годов выпуска
-def test_find_max_date_negative(headers):
+def test_find_max_date_negative_api(headers):
     base_url_loc = BASE_URL_3 + "sortField=year&sortType=1&year=1900-2028"
     response = requests.get(base_url_loc, headers=headers)
 
